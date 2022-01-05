f959962e3df1d08b9ed7575df4efdd6c45a03a0a
davidw1979 <david_weatherspoon@hotmail.com>
2022-01-05 21:01:56 +0000

    Working django framework to render index page

--- c2_main_app/views.py
+++ c2_main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.contrib import messages
+from django.views.decorators.http import require_GET
 
 # Create your views here.
+def index(request):
+
+    # Home page for committed conditioning
+    return render(request, 'c2_main_app/index.html')
