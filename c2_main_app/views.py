7a73dec02f6d3fcf0f07600eed8a881fe47c2bdb
davidw1979 <david_weatherspoon@hotmail.com>
2022-03-30 09:32:30 +0100

    Ready for initial UAT deployment. Doesn't include css for contact section.

--- c2_main_app/views.py
+++ c2_main_app/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.http import HttpResponse
@@ -6,6 +7,16 @@ from django.views.decorators.http import require_GET
 
 # Create your views here.
 def index(request):
+
+    if request.method =='POST':
+        firstname = request.POST.get('firstname')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_mail('Committed Conditioning Enquiry', f'Name:\n{firstname} {surname}\n\nEmail:\n{email}\n\n Message:\n"{message}"', email, ['info@bumptobabyyoga.co.uk'], fail_silently=False)
+        messages.success(request, "Thanks for your enquiry. We'll be in touch soon.")
+        return redirect('c2_main_app:index')
 
     # Home page for committed conditioning
     return render(request, 'c2_main_app/index.html')
