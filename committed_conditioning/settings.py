24f71aef4a00981dbc4d491e1db9754a7d2cd641
davidw1979 <david_weatherspoon@hotmail.com>
2022-04-10 20:56:15 +0100

    Now with Contact section for mobile. Needs work for desktop.

--- committed_conditioning/settings.py
+++ committed_conditioning/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.committedconditioning.com', '127.0.0.1']
 
 
 # Application definition
