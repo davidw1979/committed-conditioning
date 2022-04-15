0c55c953312980164b9855b5ec828563a754033a
davidw1979 <david_weatherspoon@hotmail.com>
2022-04-15 21:10:04 +0100

    First fully functional production push. Requires correct images and text as next step.

--- c2_main_app/static/app.js
+++ c2_main_app/static/app.js
@@ -2,17 +2,35 @@ const burger = document.querySelector('.burger');
 const sidebar = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-link');
 const socialIcons = document.querySelector('.social');
+const successMessage = document.getElementById('success-msg');
 
-console.log(burger);
-console.log(sidebar);
+// get form labels
+const formLabels = document.querySelectorAll('#new_input_form label');
 
+// get input areas of form & submit btn
+const formInputs = Array.from(document.querySelectorAll('#new_input_form .input_field'));
+const submitBtn = document.getElementById('submit');
 
+if (successMessage) {
+    const closeMessage = document.getElementById('close');
+    closeMessage.addEventListener('click', e => {
+        successMessage.classList.add("fade-out");
+        setTimeout(() => {successMessage.style.display = 'none'}, 1200);
+    })
+};
 
-
+const emailCheckre = /\S+@\S+\.\S+/
 
 /***************************************************
 **************  NAVIGATION & SIDEBAR  **************
 ***************************************************/
+
+function toggleSidebar() {
+    sidebar.classList.toggle('open');
+    navLinks.forEach(link => {
+        link.classList.toggle('fade');
+    });
+}
 
 // Trigger sidebar for mobile from burger icon
 burger.addEventListener('click', () => {
@@ -26,12 +44,7 @@ burger.addEventListener('click', () => {
 
     console.log(socialIcons);
 
-
-    // Bring in sidebar
-    sidebar.classList.toggle('open');
-    navLinks.forEach(link => {
-        link.classList.toggle('fade');
-    });
+    toggleSidebar();
 
     // Trigger burger animation
     burger.classList.toggle('burger-state');
@@ -39,11 +52,85 @@ burger.addEventListener('click', () => {
 
 function goTo(id) {
     let destination = document.querySelector(id);
-    let yCoord = destination.offsetTop -100;
+    let yCoord = destination.offsetTop -182;
     window.scrollTo({
         top: yCoord,
         behaviour: "smooth"
     });
-    sidebar.classList.remove("sidebar-active")
-    burger.classList.toggle('burger-state');
+    
+    if (sidebar.classList.contains('open')) {
+        toggleSidebar();
+        burger.classList.toggle('burger-state');
+    }
 }
+
+/*********************************************************
+ **************  FORM INPUT BEHAVIOURS  ******************
+ ********************************************************/
+
+// click onto form inputs
+formInputs.forEach(input => input.addEventListener('focus', e => {
+    e.target.style.border = 'none'
+    e.target.style.borderBottom = '2px solid white';
+    e.target.focus()
+})); 
+
+
+// make form labels also clickable
+formLabels.forEach(label => label.addEventListener('click', e => {
+    formInputs[Array.prototype.indexOf.call(formLabels, e.target)].style.border = 'none'
+    formInputs[Array.prototype.indexOf.call(formLabels, e.target)].style.borderBottom = '2px solid white';
+    formInputs[Array.prototype.indexOf.call(formLabels, e.target)].focus()
+}));
+
+// click out of form fields
+formInputs.forEach(input => input.addEventListener('blur', e => {
+    // email regex fails
+    if (e.target.id === 'email_input' && e.target.value && !emailCheckre.test(e.target.value)) {
+        e.target.style.outline = 'none';
+        e.target.style.border = '2px solid red';
+    }
+    // not the message input field but blank so revert to original state
+    else if (e.target.id !== 'message_input' && !e.target.value) {
+        e.target.style.border = 'none';
+        e.target.style.borderBottom = '2px solid var(--pri--)';
+    }
+    // message input field & blank so revert to original state
+    else if (e.target.id === 'message_input' && !e.target.value) {
+        e.target.style.border = 'none';
+        e.target.style.borderBottom = '2px solid white';
+    }
+    // not blank so full border
+    else {
+        e.target.style.border = '2px solid var(--pri--)';
+    }
+    setButton();
+})); 
+
+
+/****************************************************
+ **************  DISABLE SUBMIT BTN  ****************
+ ***************************************************/
+
+function setButton() {
+    let fieldsFull = true;
+    formInputs.forEach((input) => {
+        if (input.value.length === 0) fieldsFull = false;
+        if (input.id === 'email_input' && !emailCheckre.test(input.value)) fieldsFull = false
+    });
+    switch (fieldsFull === true) {
+        case true:
+            submitBtn.disabled = false;
+            submitBtn.style.opacity = '1'
+            break;
+        
+        case false:
+            submitBtn.disabled = true;
+            submitBtn.style.opacity = '0.5'
+            break;
+    };
+};
+
+document.onkeyup = () => {
+    setButton();
+};
--- committed_conditioning/settings.py
+++ committed_conditioning/settings.py
@@ -24,7 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
 
 ALLOWED_HOSTS = ['www.committedconditioning.com', '127.0.0.1']
 
