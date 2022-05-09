39c4a8f9714500374909cbba214effd0df4273fd
davidw1979 <david_weatherspoon@hotmail.com>
2022-05-09 23:32:09 +0100

    Missed imports for sitemap

--- committed_conditioning/urls.py
+++ committed_conditioning/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from c2_main_app.sitemaps import StaticViewSitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
