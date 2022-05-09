86ad019ebbadd52c074246e503b59e1712d71e77
davidw1979 <david_weatherspoon@hotmail.com>
2022-05-09 20:10:07 +0100

    Several changes to use whitenoise library to serve prod static files. Added to middleware, env requirements & additional to allow compression/caching and ensure dev environment serves static files same as prod.

--- committed_conditioning/settings.py
+++ committed_conditioning/settings.py
@@ -40,6 +40,8 @@ ALLOWED_HOSTS = ['www.committedconditioning.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    # Disables django serving static files in dev makes more like prod env
+    'whitenoise.runserver_nostatic', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,9 +132,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
